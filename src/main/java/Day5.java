4bfa20feaf5038e6dd7a017faa4cf8e4ef9c033c
Craig Chang <craigchang@Craigs-Mac-mini.local>
2018-12-15 01:10:03 -0800

    day 5

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Day5 {
 
@@ -7,57 +10,87 @@ public class Day5 {
         ArrayList<String> stringList = new ArrayList<String>();
         stringList = readPolymer("day5input.txt");
 
-        ArrayList<String> resultList = getNewPolymer(stringList);
+//        ArrayList<String> resultList = getNewPolymer(stringList);
+//
+//        String output = "";
+//        for(String s: resultList)
+//            output += s;
+//
+//        System.out.println(output);
 
-        String output = "";
-        for(String s: resultList)
-            output += s;
+        int shortestPolymer = getShortestPolymer(stringList);
+        System.out.println("Shortest Polymer: " + shortestPolymer);
+    }
 
-        System.out.println(output);
+    public static int getShortestPolymer(ArrayList<String> charList) {
+        ArrayList<String> newCharList = new ArrayList<String>();
+        HashMap<String, ArrayList<String>> listOfPolymers = new HashMap<String, ArrayList<String>>();
+
+        boolean unitFound = false;
+
+        String[] listOfUnits = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+        for(int j = 0; j < listOfUnits.length; j++) {
+            for (int i = 0; i < charList.size(); i++) {
+                String current = charList.get(i);
+                unitFound = findUnits(current, listOfUnits[j]);
+                if (unitFound)
+                    continue;
+                else
+                    newCharList.add(current);
+            }
+            listOfPolymers.put(listOfUnits[j], (ArrayList<String>) newCharList.clone());
+            newCharList.clear();
+        }
+
+
+        HashMap<String, Integer> listOfRemainingPolymers = new HashMap<String, Integer>();
+
+        Iterator itr = listOfPolymers.entrySet().iterator();
+        while(itr.hasNext()) {
+            Map.Entry pair = (Map.Entry) itr.next();
+            String key = (String) pair.getKey();
+            ArrayList<String> newPolymer = getNewPolymer( (ArrayList<String>) pair.getValue());
+
+            listOfRemainingPolymers.put(key, newPolymer.size());
+        }
+
+        int shortestPolymer = listOfRemainingPolymers.get("a");
+        for(String key: listOfPolymers.keySet()) {
+            if (shortestPolymer > listOfRemainingPolymers.get(key))
+                shortestPolymer = listOfRemainingPolymers.get(key);
+        }
+
+        return shortestPolymer;
+    }
+
+    public static boolean findUnits(String str, String unit) {
+        return str.equals(unit.toLowerCase()) || str.equals(unit.toUpperCase());
     }
 
     public static ArrayList<String> getNewPolymer(ArrayList<String> charList) {
-
         ArrayList<String> newCharList = new ArrayList<String>();
 
-
-        int start = 0;
-        int end = 0;
         boolean sequenceFound = false;
-        boolean sequenceDeleted = true;
+        boolean sequenceDeleted = false;
 
-
-        while(sequenceDeleted) {
-
-            start = 0;
-            end = 0;
-            sequenceFound = false;
+        do {
             sequenceDeleted = false;
-
             for (int i = 0; i < charList.size(); i++) {
                 String current = charList.get(i);
-                String next = i < (charList.size() - 1) ? charList.get(i + 1) : " ";
+                String next = (i + 1) < charList.size() ? charList.get(i + 1) : " ";
 
                 if (sequenceDeleted) {
                     newCharList.add(current);
                     continue;
                 }
 
-                if (next.toLowerCase().equals(current.toLowerCase()) && !sequenceFound &&
-                        (Character.isUpperCase(current.charAt(0)) && Character.isLowerCase(next.charAt(0)) ||
-                        Character.isLowerCase(current.charAt(0)) && Character.isUpperCase(next.charAt(0)))
-                    ) {
-                        // if current is upper and next is lower or if current is lower and next is upper
-                        sequenceFound = true;
-                        start = i;
+                sequenceFound = findSequence(current, next);
+                if (sequenceFound) {
+                    i += 1;
+                    sequenceDeleted = true;
+                    continue;
                 } else {
-                    end = i;
-                    if (end - start != 0 && sequenceFound) {
-                        i = end + 1;
-                        sequenceFound = false;
-                        sequenceDeleted = true;
-                        current = charList.get(i);
-                    }
                     newCharList.add(current);
                 }
             }
@@ -65,13 +98,17 @@ public class Day5 {
             charList = (ArrayList<String>) newCharList.clone() ;
             newCharList.clear();
 
-            if(sequenceDeleted)
-                continue;
-            else
-                break;
-        }
+            if(!sequenceDeleted) break;
+
+        } while(sequenceDeleted);
 
         return charList;
+    }
+
+    public static boolean findSequence(String str1, String str2) {
+        return str1.toLowerCase().equals(str2.toLowerCase()) &&
+                (Character.isUpperCase(str1.charAt(0)) && Character.isLowerCase(str2.charAt(0)) ||
+                        Character.isLowerCase(str1.charAt(0)) && Character.isUpperCase(str2.charAt(0)));
     }
 
     public static ArrayList<String> readPolymer(String fileName) {
